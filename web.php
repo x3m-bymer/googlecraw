fe2e6b3d22e18aeefe42b14ca8774bd7a4490f62
x3m-bymer <x3m-bymer@mail.ru>
2014-03-23 17:33:47 +0200

    boostrap

---
+++ web.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form>
+            <textarea></textarea>
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noname
+ * Date: 22.03.14
+ * Time: 18:27
+ */ 
